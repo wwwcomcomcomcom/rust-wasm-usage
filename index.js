bc6bcb646bfe8df360b1a101161640ce5cb5a122
wwwcomcomcomcom <iieiiergn@gmail.com>
2024-04-26 12:02:17 +0900

    append h1 to body with rust

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 import * as wasm from "rust-wasm";
 
 wasm.greet();
+
+const title = document.createElement("h1");
+title.innerHTML = "Hello world";
+document.body.appendChild(title);
+
+wasm.append_h1_to_body();
